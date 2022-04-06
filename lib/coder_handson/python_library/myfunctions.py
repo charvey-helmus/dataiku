15e69e98c3d1211f68bf0dc3af7dcd547014eea4
admin <chris.helmus@dataiku.com>
2022-04-06 15:53:03 -0600

    rm git ref

--- lib/coder_handson/python_library/myfunctions.py
+++
@@ -1,9 +0,0 @@
-# Define function to bin column values.
-def bin_values(revenue, v):
-    if revenue >= v:
-        val = 1
-    elif revenue < v:
-        val = 0
-    else:
-        val = revenue
-    return val
